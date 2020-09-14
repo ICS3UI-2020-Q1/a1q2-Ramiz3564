896bf292783aeaf527089f1dae1a97a31c56b041
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-09-14 16:01:10 +0000

    I have finished example question 2

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+ import java.util.Scanner;
+
 /**
- *
- * @author 
+ *this number will divide two numbers and will give you a remainder
+ * @author Zach Ramirez 
  */
 public class Main {
 
@@ -9,7 +11,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create the scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+
+    //ask user for both numbers 
+    System.out.println("Please enter your first number, this will be what you want divided"); 
+
+   int firstNumber = input.nextInt(); 
+
+    System.out.println("please enter your second number, this is what you are going to divide by");
     
+   int secondNumber = input.nextInt();
+
+   final int PRODUCT = firstNumber / secondNumber;
+   
+   final int REMAINDER = firstNumber % secondNumber; 
+
+   int productFinal = PRODUCT;
+   int remainderFinal = REMAINDER;
+   
+   System.out.println(" your final product is " + productFinal + " with a remainder of " + remainderFinal );
+
+
   }
 }
